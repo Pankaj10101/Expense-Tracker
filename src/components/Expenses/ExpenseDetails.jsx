cde0d1e0d7a2cb6fd22c9999b11b5b0b738934c7
Pankaj10101 <rkay1048@gmail.com>
2023-04-11 15:55:23 +0530

    adding delete btn

--- src/components/Expenses/ExpenseDetails.jsx
+++ src/components/Expenses/ExpenseDetails.jsx
@@ -1,9 +1,13 @@
 
 const ExpenseDetails = (props) => {
+  function deleteItem(e){
+    console.log(e.target.parentElement.parentElement.remove())
+  }
   return (
     <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>{props.amount}</div>
+            <button onClick={deleteItem}>Delete</button>
         </div>
   )
 }
