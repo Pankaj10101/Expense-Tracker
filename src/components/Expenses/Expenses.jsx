8574d3c3ac0cd13cda728f74fcc3b0d86c01b4b9
Pankaj10101 <rkay1048@gmail.com>
2023-04-11 23:55:23 +0530

    implementing filter search

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,20 +4,24 @@ import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
 const Expenses = (props) => {
-  const items = props.items;
-
   const [filteredYear, setFilteredYear] = useState("2022");
-
+  const [filteredItems, setFilteredItems] = useState(props.items)
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    const filteredItems = props.items.filter((e) => {
+    let year = e.date.getFullYear();
+    return year.toString() === selectedYear;
+  });
+  setFilteredItems(filteredItems)
   };
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {items.map((e) => {
+      {filteredItems.map((e) => {
         return (
           <ExpenseItem
             key={e.id}
