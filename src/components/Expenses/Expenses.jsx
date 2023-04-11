675bdd299990528779903def0e1c6513ea6a31b5
Pankaj10101 <rkay1048@gmail.com>
2023-04-11 23:18:28 +0530

    adding filter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
-import './Expenses.css'
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import "./Expenses.css";
 const Expenses = (props) => {
-    const items = props.items
-  return (
-    <Card className='expenses'>
-{    items.map((e)=>{
-        return <ExpenseItem  key={e.id} title ={e.title} amount= {e.anount} date ={e.date}
-/>
-    })}
-    </Card>
-  )
-}
+  const items = props.items;
 
-export default Expenses
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  return (
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {items.map((e) => {
+        return (
+          <ExpenseItem
+            key={e.id}
+            title={e.title}
+            amount={e.amount}
+            date={e.date}
+          />
+        );
+      })}
+    </Card>
+  );
+};
+
+export default Expenses;
