b4d85241081afd2b40fefbd066c4e016af7aafd2
Pankaj10101 <rkay1048@gmail.com>
2023-04-11 19:46:46 +0530

    updating data on screen

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
+import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
-
   const expenses = [
     {
       id:"e1",
       title:"Books",
-      anount:200,
+      amount:200,
       date: new Date(2023, 3,14)
     },
     {
       id:"e2",
       title:"Shoes",
-      anount:700,
+      amount:700,
       date: new Date(2023, 2,14)
     },
     {
       id:"e3",
       title:'Food',
-      anount:150,
+      amount:150,
       date: new Date(2023, 3,25)
     },
     {
       id:"e4",
       title:"Clothes",
-      anount:800,
+      amount:800,
       date: new Date(2023, 1,23)
     }
   ]
+const [newData, setNewData] = useState(expenses)
+ 
 
+  function addExpenseHandler(expense){
+    setNewData((prev)=>{
+      return [...prev, expense]
+    })
+  }
+  console.log(newData)
   return (
     <div >
-    <NewExpense/>
+    <NewExpense onAddExpense={addExpenseHandler} />
 
-    <Expenses items = {expenses}/>
+    <Expenses items = {newData}/>
 
     </div>
   );
