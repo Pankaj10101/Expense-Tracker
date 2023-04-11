4eb40fb83a35bc2e9e8cb9d0490bf069ab150333
Pankaj10101 <rkay1048@gmail.com>
2023-04-12 00:30:17 +0530

    update filter

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
-  const [filteredItems, setFilteredItems] = useState(props.items)
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    const filteredItems = props.items.filter((e) => {
-    let year = e.date.getFullYear();
-    return year.toString() === selectedYear;
-  });
-  setFilteredItems(filteredItems)
+
   };
+  const filteredItems = props.items.filter((e) => {
+    let year = e.date.getFullYear();
+    return year.toString() === filteredYear;
+  })
 
   return (
     <Card className="expenses">
@@ -21,16 +20,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredItems.map((e) => {
-        return (
-          <ExpenseItem
-            key={e.id}
-            title={e.title}
-            amount={e.amount}
-            date={e.date}
-          />
-        );
-      })}
+      <ExpensesList item = {filteredItems}/>
     </Card>
   );
 };
---
+++ src/components/Expenses/ExpensesList.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import './ExpensesList.css'
+const ExpensesList = (props) => {
+    console.log(props.item)
+    if(props.item.length===0) {
+        return <h2 className='expenses-list__fallback'>No item Found</h2>
+    }
+    return (
+    <ul className='expenses-list'>
+      {props.item.map((e) => (
+        <ExpenseItem
+          key={e.id}
+          title={e.title}
+          amount={e.amount}
+          date={e.date}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ExpensesList
